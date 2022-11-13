97fcd5959db8ab566fefe4aa0df7ee6dccc69e22
joelkarn <karn.joel@gmail.com>
2022-11-13 16:58:12 +0100

    Different movement

--- Player.gd
+++ Player.gd
@@ -12,6 +12,7 @@ var xpToLevelIncreaseRate : float = 1.2
 var interactDist : int = 70
 var vel = Vector2()
 var facingDir = Vector2()
+var walkingKeys = [0,0,0,0]
 onready var rayCast = $RayCast2D
 onready var anim = $AnimatedSprite
 
@@ -19,29 +20,57 @@ func _ready():
 	pass
 	
 func _physics_process (delta):
-  
+	
 	vel = Vector2()
-  
-	# inputs
-	if Input.is_action_pressed("move_up"):
-		vel.y -= 1
-		facingDir = Vector2(0, -1)
-	if Input.is_action_pressed("move_down"):
-		vel.y += 1
-		facingDir = Vector2(0, 1)
-	if Input.is_action_pressed("move_left"):
-		vel.x -= 1
-		facingDir = Vector2(-1, 0)
-	if Input.is_action_pressed("move_right"):
-		vel.x += 1
-		facingDir = Vector2(1, 0)
-  
-	# normalize the velocity to prevent faster diagonal movement
-	vel = vel.normalized()
-  
-	# move the player
+	var vert_move = false
+	var hori_move = false
+	var up : int = Input.is_action_pressed("move_up")
+	var down : int = Input.is_action_pressed("move_down")
+	var vert_sum = up + down
+	var right : int = Input.is_action_pressed("move_right")
+	var left : int = Input.is_action_pressed("move_left")
+	var hori_sum = right + left
+	
+	var old_keys = walkingKeys
+	walkingKeys = [up,down,right,left]
+	
+	if vert_sum == 1 and hori_sum == 1:
+		if walkingKeys == old_keys:
+			# Just keep moving the same direction
+			walk(facingDir)
+			vert_sum = 0
+			hori_sum = 0
+
+		else:
+			if facingDir.y != 0:
+				vert_sum = 0
+			else:
+				hori_sum = 0
+
+	if vert_sum == 1:
+		if up:
+			facingDir = Vector2(0, -1)
+		else:
+			facingDir = Vector2(0, 1)
+			
+		walk(facingDir)
+			
+	
+	if hori_sum == 1:
+		if right:
+			facingDir = Vector2(1, 0)
+		else:
+			facingDir = Vector2(-1, 0)
+			
+		walk(facingDir)
+			
 	move_and_slide(vel * moveSpeed, Vector2.ZERO)
 	manage_animations()
+
+func walk(dir):
+	vel.x += dir[0]
+	vel.y += dir[1]
+	
 
 func manage_animations ():
   
