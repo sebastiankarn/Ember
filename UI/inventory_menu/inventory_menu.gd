4e597cd100f7bbb44003735c2c991691dedf2a75
skarn <sebbe.karn@gmail.com>
2023-11-28 13:19:34 +0100

    Remove old ui and git addon

--- UI/inventory_menu/inventory_menu.gd
+++
@@ -1,8 +0,0 @@
-extends SlotContainer
-
-func _ready():
-	display_item_slots(Inventory.cols, Inventory.rows)
-	await get_tree().idle_frame
-	position = (get_viewport_rect().size - size) / 2
-	hide()
-	
