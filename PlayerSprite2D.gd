7e0c9dde75976326b788edeacc3b73d5cef0485c
Joel Kärn <karn.joel@gmail.com>
2023-12-26 13:12:09 +0100

    equipping now works! it is however delayed

--- PlayerSprite2D.gd
+++ PlayerSprite2D.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 
 var body_parts = ["Feet", "Legs", "Torso", "Head"]
 var base_animation_texture = preload("res://Sprites/Player/Equipment/player_animation_base.png")
+var test_texture = preload("res://Sprites/Player/player_animation_test.png")
 
 func update_animation_sprites():
 
@@ -12,20 +13,20 @@ func update_animation_sprites():
 			equipment_name = equipment_name.replace(" ", "_").to_lower()
 			var equipment_texture = load("res://Sprites/Player/Equipment/" + equipment_name + ".png")
 			var equipment_image = equipment_texture.get_image()
-
+#
 			# Assuming both images are of the same dimensions
 			for x in range(equipment_image.get_width()):
 				for y in range(equipment_image.get_height()):
 					var equipment_pixel = equipment_image.get_pixel(x, y)
 					if equipment_pixel.a > 0.0:  # If not fully transparent
 						base_image.set_pixel(x, y, equipment_pixel)
-
-
-	# Save the updated image to a file
+#
+#
+	## Save the updated image to a file
 	base_image.save_png("res://Sprites/Player/player_animation_updated.png")
-	var updated_texture = ImageTexture.new()
-	updated_texture.create_from_image(base_image)
-	self.texture = updated_texture
+	var loaded_image = Image.load_from_file("res://Sprites/Player/player_animation_updated.png")
+	var loaded_texture = ImageTexture.create_from_image(loaded_image)
+	self.texture = loaded_texture
 
 func _ready():
 	update_animation_sprites()
