acd284ce1424449bd875d1f5abd283cf19b62e3a
skarn <sebbe.karn@gmail.com>
2024-02-17 15:23:36 +0100

    Save och characterselect start

--- MainScene.gd
+++ MainScene.gd
@@ -193,3 +193,20 @@ func ItemDetermineStats(item_id, rarity, stat):
 	else:
 		stat_value = ImportData.item_data[item_id][stat]
 	return stat_value
+
+func save_game():
+	var saved_game:SavedGame = SavedGame.new()
+	
+	saved_game.player_health = player.health
+	saved_game.player_position = player.global_position
+	
+	ResourceSaver.save(saved_game, "user://savegame.tres")
+	print("SAVE GAME")
+
+func load_game():
+	var saved_game:SavedGame = load("user://savegame.tres") as SavedGame
+	
+	player.global_position = saved_game.player_position
+	player.health = saved_game.player_health
+	print("LOAD GAME")
+
---
+++ saved_game/saved_game.gd
@@ -0,0 +1,7 @@
+class_name SavedGame
+extends Resource
+
+@export var player_position:Vector2
+@export var player_health:float
+
+
