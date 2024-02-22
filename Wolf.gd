d1d8887a24af2b068c8ffac2a611a964d78a76fa
Joel Kärn <karn.joel@gmail.com>
2024-02-22 13:40:34 +0100

    removed collision from wolves

--- Wolf.gd
+++ Wolf.gd
@@ -237,7 +237,18 @@ func die():
 	
 	queue_free()
 
-func _on_Enemy_input_event(viewport, event, shape_idx):
+
+func _on_area_2d_mouse_entered():
+	get_node("/root/MainScene/CanvasLayer/MouseCursorAttack").set_as_cursor()
+	mouse_in_sprite = true
+
+
+func _on_area_2d_mouse_exited():
+	get_node("/root/MainScene/CanvasLayer/MouseCursorAttack").reset_cursor()
+	mouse_in_sprite = false
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		match event.button_index:
 			MOUSE_BUTTON_RIGHT:
@@ -249,12 +260,3 @@ func _on_Enemy_input_event(viewport, event, shape_idx):
 			MOUSE_BUTTON_LEFT:
 				if !target.hasSkillCursor:
 					target.target_enemy(self)
-
-func _on_Enemy_mouse_entered():
-	get_node("/root/MainScene/CanvasLayer/MouseCursorAttack").set_as_cursor()
-	mouse_in_sprite = true
-
-
-func _on_Enemy_mouse_exited():
-	get_node("/root/MainScene/CanvasLayer/MouseCursorAttack").reset_cursor()
-	mouse_in_sprite = false
