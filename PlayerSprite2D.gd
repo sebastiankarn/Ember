e8bb76fdc9ce62f1b36766df050e992ec8e825f2
Joel Kärn <karn.joel@gmail.com>
2024-03-19 19:48:49 +0100

    fixed rendering of cape

--- PlayerSprite2D.gd
+++ PlayerSprite2D.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 # Add textures for legs, feet, torso and head (that way helmet appears on top of torso, etc.)
-var body_parts = ["Legs", "Feet", "Torso", "Head", "Hands", "Back"]
+var body_parts = ["Back", "Legs", "Feet", "Torso", "Head", "Hands", "Back"]
 var base_animation_texture = preload("res://Sprites/Player/Equipment/player_animation_base.png")
 
 func _ready():
@@ -10,24 +10,32 @@ func _ready():
 func update_animation_sprites():
 	# Base of new image is base animations, player without anything equipped
 	var updated_image = base_animation_texture.get_image()
+	var back_behind_rendered = false
 	for body_part in body_parts:
-
 		if PlayerData.equipment_data[body_part]["Item"] != null:
 			var equipment_name = PlayerData.equipment_data[body_part]["Stats"]["Name"]
 			equipment_name = equipment_name.replace(" ", "_").to_lower()
 			if equipment_name != "cape":
 				add_texture(equipment_name, updated_image)
 			else:
-				add_texture("cape_behind", updated_image)
-				add_texture("cape_front", updated_image)
+				if not back_behind_rendered:
+					add_texture("cape_behind", updated_image)
+					back_behind_rendered = true
+					add_texture("base_anim", updated_image)
+				else:
+					add_texture("cape_front", updated_image)
 
 	var updated_texture = ImageTexture.create_from_image(updated_image)
 	self.texture = updated_texture
 
 
 func add_texture(equipment_name, updated_image):
+	var equipment_texture = Texture2D
+	if equipment_name == "base_anim":
+		equipment_texture = base_animation_texture
 	# Add texture of equpment on top of updated_image
-	var equipment_texture = load("res://Sprites/Player/Equipment/" + equipment_name + ".png")
+	else:
+		equipment_texture = load("res://Sprites/Player/Equipment/" + equipment_name + ".png")
 	var equipment_image = equipment_texture.get_image()
 
 	for x in range(equipment_image.get_width()):
