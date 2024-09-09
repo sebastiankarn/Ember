76fe09faea7761a2bdc9b25c8d005f7864f5b793
skarn <sebbe.karn@gmail.com>
2024-09-09 17:47:17 +0200

    Fireplace och stänga karta

---
+++ exit_button_map.gd
@@ -0,0 +1,6 @@
+extends TextureButton
+
+
+
+func _on_pressed() -> void:
+	pass # Replace with function body.
---
+++ world_map.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_texture_button_pressed() -> void:
+	hide()
