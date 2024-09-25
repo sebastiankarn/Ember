741c8d7a99f9b78f688b1052782e17283a8d7975
Joel Kärn <karn.joel@gmail.com>
2024-09-25 21:41:11 +0200

    fixed map and minimap to copy its tilemaplayer nodes from main scene

---
+++ MapTileMapLayers.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+
+func _ready():
+
+	# Retrieve the MainTileMapLayers node
+	var main_tilemap_layers = get_node("/root/MainScene/MainTileMapLayers")
+	
+	# Load the TileSet
+	var my_tileset = preload("res://MapTileSet.tres")
+	
+	# Clone each necessary TileMapLayer
+	for layer in ["Ground1", "Ground2", "Walls"]:
+		var original_layer = main_tilemap_layers.get_node(layer)
+		var cloned_layer = original_layer.duplicate()
+		cloned_layer.tile_set = my_tileset
+		add_child(cloned_layer)
